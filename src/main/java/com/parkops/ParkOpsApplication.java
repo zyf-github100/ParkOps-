503046132eb3fcf5fb609285ee20f6a278aa91e0
zyf-github100 <159005991+zyf-github100@users.noreply.github.com>
2026-01-21 09:56:13 +0800

    Initialize ParkOps Spring Boot skeleton

---
+++ src/main/java/com/parkops/ParkOpsApplication.java
@@ -0,0 +1,12 @@
+package com.parkops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkOpsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParkOpsApplication.class, args);
+    }
+}
